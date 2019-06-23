374a39ee348129db943c1672b98dcac487896b24
uesseu <sheepwing@kyudai.jp>
2019-06-24 01:20:21 +0900

    Now, it can run, and may be useful???

--- morse.py
+++ morse.py
@@ -1,196 +1,273 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.signal import cwt, morlet
-from scipy.fftpack import fft, ifft
-from scipy import sign
-from math import e, pi
-from typing import Union
+from numpy import linalg, sqrt, e
 from mpl_toolkits.mplot3d import Axes3D
-from numpy import linalg, sqrt
-plot: bool = True
+import matplotlib.pyplot as plt
+from scipy.fftpack import ifft
+from typing import Union, List
+from mne.time_frequency import tfr, morlet
 
 
 class Morse:
-    def __init__(self):
-        self.r = 3
-        self.b = 20
-        self.l = 7
-        self.a = 2 * (e * self.r / self.b) ** (self.b / self.r)
+    def __init__(self, sfreq: float = 1000, b: float = 17.5, r: float = 3,
+                 length: float = 10, accuracy: float = 1) -> None:
+        '''
+        Generator of Generalized Morse Wavelets.
+        It is compatible with mne-python.
+        It generates argument Ws for mne.time_frequency.tfr.cwt.
+        Example.
+        >>> morse = Morse(1000).beta(20)
+        >>> freq = 60
+        >>> time = np.arange(0, 0.3, 0.001)
+        >>> sin = np.array([np.sin(time * freq * 2 * np.pi)])
+        >>> result = morse.power(sin, range(1, 100))
+        >>> plt.imshow(result, cmap='RdBu_r')
+        >>> plt.gca().invert_yaxis()
+        >>> plt.title('CWT of 60Hz sin wave')
+        >>> plt.show()
+
+        Parameters
+        ----------
+        sfreq: float | Sampling frequency.
+            This behaves like sfreq of mne-python.
+        b: float | beta value
+        r: float | gamma value. 3 may be good value.
+        accuracy: float | Accurancy paramater.
+            Because, Morse Wavelet needs Fourier Transform,
+            length of wavelet changes but it's tiring to detect. :(
+            Low frequency causes bad wave.
+            Please check wave by Morse.plot(freq) before use it.
+            If wave is bad, large accuracy can help you.(But needs cpu power)
+        length: float | Length paramater.
+            Too long wavelet causes slow calculation.
+            This param is cutting threshould of wave.
+            Peak wave * length is the length of wavelet.
+
+        Returns
+        -------
+        Morse instance its self.
+        '''
+        self._peak: float
+        self.r: float = r
+        self.b: float = b
+        self.length: float = length
+        self.accuracy: float = accuracy
+        self.a: float = 2 * (e * self.r / self.b) ** (self.b / self.r)
+        self.sfreq: float = sfreq
         self._setup_peak()
 
-    def _setup_peak(self):
-        self.peak = (self.b / self.r) ** (1 / self.r) / np.pi
+    def _setup_peak(self) -> None:
+        self.peak: float = (self.b / self.r) ** (1 / self.r)
 
     def beta(self, b: float) -> 'Morse':
+        '''
+        Set gamma value of MorseWavelet.
+        If it is 17.5, MorseWavelets may resembles MorseWavelet with sigma 7.
+
+        Parameters
+        ----------
+        r: float | gamma value
+
+        Returns
+        -------
+        Morse instance its self.
+        '''
         self.b = b
         self._setup_peak()
         return self
 
     def gamma(self, r: float) -> 'Morse':
+        '''
+        Set gamma value of MorseWavelet.
+        Good value may be 3.
+
+        Parameters
+        ----------
+        r: float | gamma value
+
+        Returns
+        -------
+        Morse instance its self.
+        '''
         self.r = r
         self._setup_peak()
         return self
 
-    def ln(self, l: float) -> 'Morse':
-        self.peak = l
-        # self.peak = self.b ** (1 / self.r)
-        self.r = np.log(self.b) / np.log(self.peak)
-        # self.b = 1 / np.log(l)
-        # self.b = 0.05 * l ** self.r
-        return self
+    def wavelet(self, freq: float = 10) -> np.ndarray:
+        '''
+        Make morse wavelet.
+        It returnes one freq wavelet only, and so it may be useless.
 
-    def make_wave(self, sfreq=1000, freq=10) -> 'Morse':
-        # use 20rad
-        sigma_t = 7 / (2.0 * np.pi * 1)
-        self.wave_len = np.arange(0., 5. * sigma_t, 1.0 / sfreq)
-        # self.wave_len = self.l / (2.0 * np.pi) 
-        # self.wave_len = 1
+        Parameters
+        ----------
+        freq: float | Frequency. If frequency is too small,
+            it returnes bad wave easily.
+            For example, sfreq=1000, freq=3 it returnes bad wave.
+            If you want good wave, you must set large accuracy, and length
+            when you make this instance.
+
+        Returns
+        -------
+        MorseWavelet: np.ndarray
+        '''
         self._setup_peak()
-        # wave_len = 11.15  # sec
-        # this is not raw formula
-        # length of one sample frequency
+        one = 1 / freq / self.accuracy
+        total = self.sfreq / freq
+        return self._ifft(total, one, freq)
 
-        one = 1 / sfreq * (self.b / self.r) ** (1 / self.r)
-        self.one = one
-        total = 5 * sigma_t * self.peak * sfreq
-        #total = 100
-        w = np.arange(0, total, one)
-        self.w = w
-        step = np.heaviside(w, w)
-        # the morse wavelet
-        # This is the raw formula
-        self.wave = step * self.a * w ** self.b * e ** (-1 * w ** self.r)
+    def wavelets(self,
+                 freqs: Union[List[float], range, np.ndarray]) -> np.ndarray:
+        '''
+        Make morse wavelet.
+        It returnes list of wavelet, and it is compatible with mne-python.
+        (As argument of Ws of mne.time_frequency.tfr.cwt)
 
-        # inverse fft
-        # This is the raw formula
-        # self.morse_wave = ifft(self.wave)[:int(total / one / 2)]
-        self.morse_wave = ifft(self.wave)[:int(5 * sigma_t * sfreq / freq)]
-        print(self.morse_wave.shape)
-        # contactnate minus value
-        self.morse_wave = np.hstack((np.conj(np.flip(self.morse_wave)),
-                                     self.morse_wave))
+        Parameters
+        ----------
+        freqs: List[float] | Frequency. If frequency is too small,
+            it returnes bad wave easily.
+            For example, sfreq=1000, freq=3 it returnes bad wave.
+            If you want good wave, you must set large accuracy, and length
+            when you make this instance.
+
+        Returns
+        -------
+        MorseWavelet: np.ndarray
+        '''
+        return list(map(self.wavelet, freqs))
+
+    def _ifft(self, total: float, one: float, freq: float) -> np.ndarray:
+        '''
+        Private method to make morse_wavelet.
+        '''
+        w: np.ndarray = np.arange(0, total, one)
+        step: np.ndarray = np.heaviside(w, w)
+        wave: np.ndarray = (step * self.a * w ** self.b *
+                            e ** ((self.b / self.r) * (1 - w ** self.r))
+                            )
+        morse_wavelet: np.ndarray = ifft(wave)
+        half = int(morse_wavelet.shape[0])
+        band = int(half / 2 / freq * self.length)
+        start: int = half - band if band < half // 2 else half // 2
+        stop: int = half + band if band < half // 2 else half // 2 * 3
+        total_wavelet: np.ndarray = np.hstack((np.conj(np.flip(morse_wavelet)),
+                                               morse_wavelet))
+        morse_wavelet = total_wavelet[start: stop]
         # normalize
-        self.morse_wave /= sqrt(0.5) * linalg.norm(self.morse_wave.ravel())
-        return self
+        morse_wavelet /= sqrt(0.5) * linalg.norm(morse_wavelet.ravel())
+        return morse_wavelet
 
-    def plot_wave(self):
-        plt.plot(self.wave)
-        plt.show()
+    def cwt(self, wave: np.ndarray,
+            freqs: Union[List[float], range, np.ndarray], use_fft: bool = True,
+            mode: str = 'same', decim: float = 1) -> np.ndarray:
+        '''
+        Run cwt of mne-python.
 
-    def plot_wavelet(self):
-        plt.plot(self.morse_wave)
-        plt.show()
+        Parameters
+        ----------
+        freqs: float | Frequencies. Before use this, please run plot.
 
-    def wavelet(self):
-        return self.morse_wave
+        Returns
+        -------
+        Result of cwt. Complex np.ndarray.
+        '''
+        return tfr.cwt(wave, self.wavelets(range(1, 100)), use_fft=use_fft,
+                       mode=mode, decim=decim).mean(axis=0)
 
+    def power(self, wave: np.ndarray,
+              freqs: Union[List[float], range, np.ndarray]) -> np.ndarray:
+        '''
+        Run cwt of mne-python, and compute power.
 
-def mne_morlet(sfreq, freqs, n_cycles=7.0, sigma=None, zero_mean=False):
-    """Compute Morlet wavelets for the given frequency range.
+        Parameters
+        ----------
+        freqs: float | Frequencies. Before use this, please run plot.
 
-    Parameters
-    ----------
-    sfreq : float
-        The sampling Frequency.
-    freqs : array
-        frequency range of interest (1 x Frequencies)
-    n_cycles: float | array of float, default 7.0
-        Number of cycles. Fixed number or one per frequency.
-    sigma : float, default None
-        It controls the width of the wavelet ie its temporal
-        resolution. If sigma is None the temporal resolution
-        is adapted with the frequency like for all wavelet transform.
-        The higher the frequency the shorter is the wavelet.
-        If sigma is fixed the temporal resolution is fixed
-        like for the short time Fourier transform and the number
-        of oscillations increases with the frequency.
-    zero_mean : bool, default False
-        Make sure the wavelet has a mean of zero.
+        Returns
+        -------
+        Result of cwt. np.ndarray.
+        '''
+        result: np.ndarray = self.cwt(wave, freqs)
+        return np.abs(result * np.conj(result))
 
-    Returns
-    -------
-    Ws : list of array
-        The wavelets time series.
-    """
-    Ws = list()
-    n_cycles = np.atleast_1d(n_cycles)
-
-    freqs = np.array(freqs)
-    if np.any(freqs <= 0):
-        raise ValueError("all frequencies in 'freqs' must be "
-                         "greater than 0.")
-
-    if (n_cycles.size != 1) and (n_cycles.size != len(freqs)):
-        raise ValueError("n_cycles should be fixed or defined for "
-                         "each frequency.")
-    for k, f in enumerate(freqs):
-        if len(n_cycles) != 1:
-            this_n_cycles = n_cycles[k]
-        else:
-            this_n_cycles = n_cycles[0]
-        # fixed or scale-dependent window
-        if sigma is None:
-            sigma_t = this_n_cycles / (2.0 * np.pi * f)
-        else:
-            sigma_t = this_n_cycles / (2.0 * np.pi * sigma)
-        # this scaling factor is proportional to (Tallon-Baudry 98):
-        # (sigma_t*sqrt(pi))^(-1/2);
-        t = np.arange(0., 5. * sigma_t, 1.0 / sfreq)
-        t = np.r_[-t[::-1], t[1:]]
-        oscillation = np.exp(2.0 * 1j * np.pi * f * t)
-        gaussian_enveloppe = np.exp(-t ** 2 / (2.0 * sigma_t ** 2))
-        if zero_mean:  # to make it zero mean
-            real_offset = np.exp(- 2 * (np.pi * f * sigma_t) ** 2)
-            oscillation -= real_offset
-        W = oscillation * gaussian_enveloppe
-        W /= sqrt(0.5) * linalg.norm(W.ravel())
-        Ws.append(W)
-    return Ws
+    def plot(self, freq: float, show: bool = True) -> plt.figure:
+        morse = self.wavelet(freq)
+        fig = plt.figure(figsize=(6, 8))
+        ax = fig.add_subplot(311)
+        ax.plot(np.arange(0, morse.shape[0], 1),
+                morse,
+                label='morse')
+        ax1 = fig.add_subplot(312, projection='3d')
+        ax1.scatter3D(morse.real,
+                      np.arange(0, morse.shape[0], 1),
+                      morse.imag,
+                      label='morse')
+        ax.set_title('Generalized Morse Wavelet')
+        ax2 = fig.add_subplot(313)
+        ax2.set_title('Caution')
+        ax2.text(0.05, 0.5, 'If wave continues to side of the window,'
+                 'wave is bad.\n'
+                 'Please set larger value to param "accuracy" and "length"\n'
+                 'It become bad easily when frequency is low.')
+        ax2.tick_params(labelbottom=False,
+                        labelleft=False,
+                        labelright=False,
+                        labeltop=False, bottom=False,
+                        left=False, right=False, top=False)
+        if show:
+            plt.show()
+        fig
 
 
-n_cycles = 7
-morse = Morse().beta(17.5).make_wave()
-if plot:
-    plt.plot(morse.wave)
-    plt.show()
-    plt.plot(morse.wavelet())
-    plt.show()
-    plt.plot(np.abs(fft(morse.wavelet())))
+def test() -> None:
+    morse = Morse(1000).beta(20)
+    freq = 60
+    time = np.arange(0, 0.3, 0.001)
+    sin = np.array([np.sin(time * freq * 2 * np.pi)])
+    result = morse.power(sin, range(1, 100))
+    plt.imshow(result, cmap='RdBu_r')
+    plt.gca().invert_yaxis()
+    plt.title('CWT of 60Hz sin wave')
     plt.show()
 
-mm = mne_morlet(1000, [10], n_cycles=n_cycles, zero_mean=True)[0]
-go = mne_morlet(1000, [10], n_cycles=n_cycles)[0]
-morse_wave = morse.wavelet()
-morse.plot_wave()
-morse.plot_wavelet()
 
-fig = plt.figure()
-ax = fig.add_subplot(211)
-ax.plot(np.arange(0, morse_wave.shape[0], 1),
-        morse_wave,
-        label='morse')
-ax.plot(np.arange(0, mm.shape[0], 1),
-        mm,
-        label='morlet')
-ax.plot(np.arange(0, go.shape[0], 1),
-        go,
-        label='go')
-ax1 = fig.add_subplot(212, projection='3d')
-ax1.scatter3D(morse_wave.real,
-              np.arange(0, morse_wave.shape[0], 1),
-              morse_wave.imag,
-              label='morse')
-ax1.scatter3D(mm.real,
-              np.arange(0, mm.shape[0], 1),
-              mm.imag,
-              label='morlet')
-ax1.scatter3D(go.real,
-              np.arange(0, mm.shape[0], 1),
-              go.imag,
-              label='gobar')
+def test3d() -> None:
+    go = morlet(1000, [10])[0]
+    mm = morlet(1000, [10], zero_mean=True)[0]
+    morse = Morse(1000, 17.5, 3).wavelet(10)
+    fig = plt.figure()
+    ax = fig.add_subplot(211)
+    half_morse = morse.shape[0] / 2
+    half_mm = mm.shape[0] / 2
+    ax.plot(np.arange(-half_morse, half_morse, 1),
+            morse,
+            label='morse')
+    ax.plot(np.arange(-half_mm, half_mm, 1),
+            mm,
+            label='morlet')
+    ax.plot(np.arange(-half_mm, half_mm, 1),
+            go,
+            label='go')
+    ax1 = fig.add_subplot(212, projection='3d')
+    ax1.scatter3D(morse.real,
+                  np.arange(-half_morse, half_morse, 1),
+                  morse.imag,
+                  label='morse')
+    ax1.scatter3D(mm.real,
+                  np.arange(-half_mm, half_mm, 1),
+                  mm.imag,
+                  label='morlet')
+    ax1.scatter3D(go.real,
+                  np.arange(-half_mm, half_mm, 1),
+                  go.imag,
+                  label='gobar')
+    handler, label = ax.get_legend_handles_labels()
+    handler1, label1 = ax1.get_legend_handles_labels()
+    ax.legend(label+label1, loc='upper right')
+    ax.set_title('morse and morlet')
+    plt.show()
 
-handler, label = ax.get_legend_handles_labels()
-handler1, label1 = ax1.get_legend_handles_labels()
-ax.legend(label+label1, loc='upper right')
-ax.set_title('morse and morlet')
-plt.show()
+
+if __name__ == '__main__':
+    print('Test Run')
+    test()
+    Morse().plot(10)
