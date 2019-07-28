14982a43aad1b6d7990db945f5a5a244997fa3dc
uesseu <sheepwing@kyudai.jp>
2019-07-28 23:05:12 +0900

    compile

--- nin_wavelets/__init__.py
+++ nin_wavelets/__init__.py
@@ -1,2 +1,14 @@
+from pathlib import Path
+from os import chdir, getcwd
+from subprocess import run
+
+current = getcwd()
+if (Path(__file__).parent / 'base.c').exists() is False:
+    chdir(Path(__file__).parent.parent)
+    print('Is it first time import?')
+    print('Compiling')
+    run(['python', 'setup_cy.py', 'build_ext', '--inplace'])
+
+
 from .wavelets import WaveletBase
 from .morse import Morse, MorseMNE, Morlet
